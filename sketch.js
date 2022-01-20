dd6dbc5b899940480280b15648da05a620d54ee3
KashyapAnkitaM <96985462+KashyapAnkitaM@users.noreply.github.com>
2022-01-20 14:29:25 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,54 @@
+var Runner,RunnerImg
+var path,pathImg
+
+function preload() {
+// pre-load
+
+  pathImg = loadImage("path.png") ;
+  RunnerImg = loadImage("Runner-1.png");
+
+}
+
+function setup() {
+
+createCanvas(400,600)
+background("blue");
+// moving background
+   path = createSprite(200,100,150,400);
+   path.addImage(pathImg);
+   path.y = path.height/2 ; 
+   path.velocityY = 3 ; 
+   path.scale = 1.2 ;
+
+   
+   Runner = createSprite(200,545,20,20);
+   Runner.addImage(RunnerImg);
+   Runner.scale = 0.05 ; 
+}
+
+
+
+
+  
+
+function draw() {
+    background("blue") ; 
+    drawSprites();
+
+ //code to reset the background
+    if(path.y > 400) {
+      path.y = height/2
+    }
+
+//Add the condition to make the Runner move left and right
+
+if(keyDown(LEFT_ARROW)){
+  Runner.x = Runner.x - 5 ;
+}
+
+
+if(keyDown(RIGHT_ARROW)){
+  Runner.x = Runner.x + 5 ;
+}  
+}
+
